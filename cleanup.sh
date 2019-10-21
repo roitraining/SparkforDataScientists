ac894e7f2b68be78e191dbfa7b2a2d7dfdf80b28
Joseph Gagliardo <joey@me.com>
2019-10-21 16:40:08 -0400

    0.1.10 First Run 2019-10-22

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+rm -r ~/file1.txt
+rm -r ~/ROI/Spark/__pycache__
+rm -r ~/ROI/Spark/bank_csv_*
+rm -r ~/ROI/Spark/CreditCard.json
+rm -r ~/ROI/Spark/Orders_LineItems.json
+rm -r ~/ROI/Spark/TerritoryRegion.json
+rm -r ~/ROI/Spark/testsave
+rm -r ~/ROI/Spark/flower_photos
+rm ~/ROI/Spark/model-full.h5
+rm ~/ROI/Spark/simple-binary-classification
+
+
